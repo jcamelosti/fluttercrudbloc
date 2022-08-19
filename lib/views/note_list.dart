580e0cf1a884c5026d8930a92f409e5af6e003b0
Josué Camelo <cloudone@josuecamelo.com>
2022-08-18 21:25:31 -0300

    Listagem de Itens

--- lib/views/note_list.dart
+++ lib/views/note_list.dart
@@ -2,11 +2,16 @@ import 'package:crudblocsqlite/cubits/note_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../cubits/note_state.dart';
+import '../models/note.dart';
 import 'note_edit.dart';
 
 class NoteListPage extends StatelessWidget {
   const NoteListPage({Key? key}) : super(key: key);
 
+  // o NotesCubit que foi criado e providenciado para o MaterialApp eh recuperado
+  // via construtor .value e executa a funcao de buscar as notas,
+  // ou seja, novas instancias nao usam o .value, instancias existentes sim
   @override
   Widget build(BuildContext context) {
     return BlocProvider.value(
@@ -58,10 +63,12 @@ class DocumentosView extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(),
+      body: const _Content(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
+          // como o FAB cria uma nota nova, a nota nao eh parametro recebido
+          // na tela de edicao
           Navigator.push(
             context,
             MaterialPageRoute(
@@ -69,6 +76,112 @@ class DocumentosView extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<NotesCubit>().state;
+    // a descricao dos estados esta no arquivo notes_state
+    // os estados nao tratados aqui sao utilizados na tela de edicao da nota
+    // print('notelist ' + state.toString());
+    if (state is NotesInitial) {
+      return const SizedBox();
+    } else if (state is NotesLoading) {
+      return const Center(
+        child: CircularProgressIndicator.adaptive(),
+      );
+    } else if (state is NotesLoaded) {
+      //a mensagem abaixo aparece se a lista de notas estiver vazia
+      if (state.notes!.isEmpty) {
+        return const Center(
+          child: Text('Não há notas. Clique no botão abaixo para cadastrar.'),
+        );
+      } else {
+        return _NotesList(state.notes);
+      }
+    } else {
+      return const Center(
+        child: Text('Erro ao recuperar notas.'),
+      );
+    }
+  }
+}
+
+class _NotesList extends StatelessWidget {
+  const _NotesList(this.notes, {Key? key}) : super(key: key);
+  final List<Note>? notes;
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        for (final note in notes!) ...[
+          Padding(
+            padding: const EdgeInsets.all(2.5),
+            child: ListTile(
+              tileColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              title: Text(note.title),
+              subtitle: Text(
+                note.content,
+              ),
+              trailing: Wrap(children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        // a nota existente eh enviada como parametro para a
+                        // tela de edicao preencher os campos automaticamente
+                          builder: (context) => NoteEditPage(note: note)),
+                    );
+                  },
+                ),
+                IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      // excluir nota atraves do id
+                      showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: const Text('Excluir Nota'),
+                          content: const Text('Confirmar operação?'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text('Cancelar'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                context.read<NotesCubit>().excluirNota(note.id);
+                                Navigator.pop(context);
+                                ScaffoldMessenger.of(context)
+                                  ..hideCurrentSnackBar()
+                                  ..showSnackBar(const SnackBar(
+                                    content: Text('Nota excluída com sucesso'),
+                                  ));
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        ),
+                      );
+                    }),
+              ]),
+            ),
+          ),
+          // const Divider(
+          //   height: 2,
+          // ),
+        ],
+      ],
     );
   }
 }

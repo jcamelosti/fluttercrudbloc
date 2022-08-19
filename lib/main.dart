03162d8a4719274ce6ddeb82c75cd47191944c83
Josué Camelo <cloudone@josuecamelo.com>
2022-08-18 21:10:21 -0300

    Configuração dos Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:crudblocsqlite/db/database_provider.dart';
 import 'package:crudblocsqlite/views/note_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubits/note_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +15,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return RepositoryProvider(
+      create: (context) => DatabaseProvider.instance,
+      child: BlocProvider(
+        create: (context) =>
+            NotesCubit(databaseProvider: DatabaseProvider.instance),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const NoteListPage(),
+        ),
       ),
-      home: const NoteListPage(),
     );
   }
 }
